4793a2b6a377b099ac63b2e25ee3af2a35eb6f24
Your Name <vipa4ka@gmail.com>
2022-10-30 20:24:48 +0200

    init webpack

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './css/index.css';
+// import css from "./css/index.css";
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = 'Hello';
+  element.style.fontSize = '150px'
+
+    element.style.color = 'tomato'
+
+  
+
+  return element;
+}
+
+document.body.appendChild(component());
