d4f03302955f60bac4c685d922234ea165bfdc6a
Your Name <vipa4ka@gmail.com>
2022-10-31 15:57:06 +0200

    install  CleanWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -24,9 +25,10 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new MiniCssExtractPlugin({ }),
+    new MiniCssExtractPlugin({}),
+    new CleanWebpackPlugin(),
   ],
-  devServer: {
+  devServer: { 
     port: 9000,
     open: true,
   }
