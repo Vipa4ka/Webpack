cb0a2cb055cf1b29e4ef11b6646812ffe45bd41c
Your Name <vipa4ka@gmail.com>
2022-10-31 16:10:54 +0200

    finish install

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import './css/index.scss';
 
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = 'Hello';
-  element.style.fontSize = '150px'
-
-    element.style.color = 'tomato'
-
-  
-
-  return element;
-}
-
-document.body.appendChild(component());
