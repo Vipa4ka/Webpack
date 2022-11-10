1e142859eb66bf78de88b60b00864a0bd18ef64f
Your Name <vipa4ka@gmail.com>
2022-11-10 12:39:59 +0200

    fix: change package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: './src/index.js',
   output: {
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: 'food-service.js',
   },
   module: {
     rules: [
       {
-        test: /\.scss$/i,
-       use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }, 
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.hbs$/,
+        use: 'handlebars-loader',
+      },
     ],
   },
   plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new MiniCssExtractPlugin({}),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
     new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: './src/images', to: 'images' }],
+    }),
   ],
-  devServer: { 
-    port: 9000,
-    open: true,
-  }
 };
